2bef99e6420167f0ad6067c7b42bd73c3b5244ee
Paweł Kopiczko <kopiczko@gmail.com>
2019-09-25 13:58:01 +0100

    improve test diff

--- format_test.go
+++ format_test.go
@@ -100,28 +100,28 @@ func Test_format(t *testing.T) {
 			errorMatcher: IsJSONObjectParse,
 		},
 		{
-			name:         "case 9: deal with number values",
-			text:         `{"caller":"github.com/giantswarm/opsctl/command/deploy/command.go:169","level":"error","time":"2019-09-06T12:40:27.581957+00:00","verbosity":0}`,
-			expectedOut:  `E 09/06 12:40:27 | opsctl/command/deploy/command.go:169 | verbosity=0`,
-			errorMatcher: nil,
-		},
-		{
-			name:         "case 10: ignore `null`",
+			name:         "case 9: ignore `null`",
 			text:         `null`,
 			expectedOut:  ``,
 			errorMatcher: IsJSONObjectParse,
 		},
 		{
-			name:         "case 11: ignore numbers",
+			name:         "case 10: ignore numbers",
 			text:         `7`,
 			expectedOut:  ``,
 			errorMatcher: IsJSONObjectParse,
 		},
 		{
-			name:         "case 12: ignore empty lines",
+			name:         "case 11: ignore empty lines",
 			text:         ``,
 			expectedOut:  ``,
 			errorMatcher: IsJSONObjectParse,
+		},
+		{
+			name:         "case 12: deal with number values",
+			text:         `{"caller":"github.com/giantswarm/opsctl/command/deploy/command.go:169","level":"error","time":"2019-09-06T12:40:27.581957+00:00","verbosity":0}`,
+			expectedOut:  `E 09/06 12:40:27 | opsctl/command/deploy/command.go:169 | verbosity=0`,
+			errorMatcher: nil,
 		},
 	}
 
